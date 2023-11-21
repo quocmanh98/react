10d3a5e554eb84e92db6831dd35fa1495fac9ab4
Quoc Manh <manhquoc1998s@gmail.com>
2023-11-21 23:11:10 +0700

    manhtq update

--- src/App.js
+++ src/App.js
@@ -6,18 +6,19 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Quốc Mạnh
-        </p>
+        <h1 style={{ fontSize: '2em', margin: '10px 0', color: '#333' }}>Welcome to My React App</h1>
+        <p style={{ fontSize: '1.5em', color: '#555' }}>Hi, I'm Quốc Mạnh</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
+          style={{ fontSize: '1.2em', color: '#61dafb', textDecoration: 'none' }}
         >
           Learn React
         </a>
       </header>
+
     </div>
   );
 }
